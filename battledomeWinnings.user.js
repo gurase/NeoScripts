a692d4f955f28c8d55a39cce8d0d7e5aec5db782
Grace <gracemariemann@gmail.com>
2024-07-13 08:04:31 -0400

    Added plot points counter

--- battledomeWinnings.user.js
+++ battledomeWinnings.user.js
@@ -2,7 +2,7 @@
 // @author         smthngsaid
 // @name           Neopets - Battledome Prize Counter
 // @description    Keeps a count of the items and Neopoints you've won today
-// @include        http://www.neopets.com/dome/arena.phtml
+// @match        https://www.neopets.com/dome/arena.phtml
 // ==/UserScript==
 $(document).ready(function () {
     $('#arenacontainer').on('click', '.end_game', function () {
@@ -21,26 +21,33 @@ function displayTotals() {
     var storedDate = window.localStorage.getItem('battledomeDate');
     var nPCount = window.localStorage.getItem('battledomeNPCount') == null ? 0 : parseInt(window.localStorage.getItem('battledomeNPCount'));
     var itemCount = window.localStorage.getItem('battledomeItemCount') == null ? 0 : parseInt(window.localStorage.getItem('battledomeItemCount'));
+    var pointCount = window.localStorage.getItem('battledomePointCount') == null ? 0 : parseInt(window.localStorage.getItem('battledomePointCount'));
     if (storedDate != currentDate) {
         nPCount = 0;
         itemCount = 0;
+        pointCount = 0;
         window.localStorage.setItem('battledomeNPCount', nPCount);
         window.localStorage.setItem('battledomeItemCount', itemCount);
+        window.localStorage.setItem('battledomePointCount', pointCount);
         window.localStorage.setItem('battledomeDate', currentDate);
     }
     //Add any winnings and store the new totals
-    let prizes = document.querySelectorAll("#bd_rewardsloot .prizname");
+    let prizes = document.querySelectorAll("span.prizname");
     if (prizes.length > 0) {
         for (let i = 0; i < prizes.length; i++) {
             if (/\d+ Neopoints/.test(prizes[i].textContent)) {
                 let np = prizes[i].textContent.split(' ')[0];
                 nPCount += parseInt(np);
+            } else if (/\d+ Plot Points/.test(prizes[i].textContent)) {
+                let points = prizes[i].textContent.split(' ')[0];
+                pointCount += parseInt(points);
             } else {
                 itemCount += 1;
             }
         }
         window.localStorage.setItem('battledomeNPCount', nPCount);
         window.localStorage.setItem('battledomeItemCount', itemCount);
+        window.localStorage.setItem('battledomePointCount', pointCount);
     }
     //Display the totals
     let counter = document.createElement("span");
@@ -50,7 +57,12 @@ function displayTotals() {
     let separator = document.createElement("span");
     separator.textContent = ' | ';
     separator.setAttribute("style", "font-weight: normal;");
+    let separator2 = document.createElement("span");
+    separator2.textContent = ' | ';
+    separator2.setAttribute("style", "font-weight: normal;");
     let np = document.createElement("strong");
     np.textContent = 'NP: ' + nPCount;
-    rewardsBox.children[1].appendChild(counter).appendChild(items).appendChild(separator).appendChild(np);
+    let points = document.createElement("strong");
+    points.textContent = 'Plot: ' + pointCount;
+    rewardsBox.children[1].appendChild(counter).appendChild(items).appendChild(separator).appendChild(np).appendChild(separator2).appendChild(points);
 }
